3e542535e4a4741d9e109c22e695fa63218fb8fe
AbdelRhman Ibrahim <114269455+AbdelrhmanJaber@users.noreply.github.com>
2023-08-19 15:23:14 +0200

    Create MSTK_config.h

---
+++ 2- MCAL/05- STK/MSTK_config.h
@@ -0,0 +1,27 @@
+/*
+ *   file : MSTK_config.h
+ *   Date : 19/8/2023
+ *   Author : Abdelrahman Ibrahim
+ *
+ * */
+
+
+/******************************Header Guard*******************************************/
+
+
+#ifndef MCAL_MSTK_MSTK_CONFIG_H_
+#define MCAL_MSTK_MSTK_CONFIG_H_
+
+
+#define PROCCESOR_CLK_AHB          1
+#define AHB_8                      2
+
+#define CLK_SOURCE_CONFIG                 AHB_8
+
+
+#define  HSE                         1
+#define  HSI                         2
+
+#define CLK_RCC_SOURCE               HSE
+
+#endif /* MCAL_MSTK_MSTK_CONFIG_H_ */
