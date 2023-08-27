6c68c90ebe52cfa855a06243574be11e26f4ad17
AbdelRhman Ibrahim <114269455+AbdelrhmanJaber@users.noreply.github.com>
2023-08-27 22:10:26 +0300

    final

--- 2- MCAL/06- UART/MUSART_interface.h
+++ 2- MCAL/06- UART/MUSART_interface.h
@@ -17,10 +17,6 @@ typedef enum{
 
 void MUSART_voidInit(USART_SELCTION_t copy_enumUSART);
 
-void MUSART_voidEnable(void);
-
-void MUSART_voidDisable(void);
-
 void MUSART_voidSendByte(USART_SELCTION_t copy_enumUSART , u8 copy_u8ByteToSend);
 
 u8 MUSART1_u8ReceiveByte(void);
@@ -36,6 +32,30 @@ u8 MUSART1_Pu8RecieveStringSynch(s8* word);
 u8 MUSART2_Pu8RecieveStringSynch(s8* word);
 
 u8 MUSART6_Pu8RecieveStringSynch(s8* word);
+
+void MUSART1_voidSendByteAsynch(u8 copy_u8ByteToSend);
+
+void MUSART2_voidSendByteAsynch(u8 copy_u8ByteToSend);
+
+void MUSART6_voidSendByteAsynch(u8 copy_u8ByteToSend);
+
+void MUSART1_voidSendStringAsynch(s8* copy_Pu8StringToSend);
+
+void MUSART2_voidSendStringAsynch(s8* copy_Pu8StringToSend);
+
+void MUSART6_voidSendStringAsynch(s8* copy_Pu8StringToSend);
+
+void MUSART1_voidRecieveByteAsynch(u8* data);
+
+void MUSART2_voidRecieveByteAsynch(u8* data);
+
+void MUSART6_voidRecieveByteAsynch(u8* data);
+
+void MUSART1_voidRecieveStringAsynch(s8* copy_Pu8StringToRecieve);
+
+void MUSART2_voidRecieveStringAsynch(s8* copy_Pu8StringToRecieve);
+
+void MUSART6_voidRecieveStringAsynch(s8* copy_Pu8StringToRecieve);
 
 void MUSART_voidSetCallBack(USART_SELCTION_t copy_enumUSART , void(*Copy_Pf)(void));
 
--- 2- MCAL/06- UART/MUSART_prog.c
+++ 2- MCAL/06- UART/MUSART_prog.c
@@ -10,29 +10,80 @@
 #include"../../LIB/BIT_Math.h"
 
 
-#include"../MGPIO/MGPIO_interface.h"
 #include "MUSART_config.h"
 #include "MUSART_interface.h"
 #include "MUSART_private.h"
 
+/*array of pointers to function*/
+
 void(*USART_PF[3])(void) = {NULL , NULL, NULL};
 
+/*global variables to send data asynchronous*/
+
+static u16 USART1_global_data_to_send;
+static u16 USART2_global_data_to_send;
+static u16 USART6_global_data_to_send;
+
+/*global pointers to send string*/
+static s8* USART1_globalStringTX = NULL;
+static s8* USART2_globalStringTX = NULL;
+static s8* USART6_globalStringTX = NULL;
+
+/*global pointers to receive data*/
+static u8* USART1_globalByteRX = NULL;
+static u8* USART2_globalByteRX = NULL;
+static u8* USART6_globalByteRX = NULL;
+
+/*global pointers to receive string*/
+static s8* USART1_globalReceiveRX = NULL;
+static s8* USART2_globalReceiveRX = NULL;
+static s8* USART6_globalReceiveRX = NULL;
+
+/*private init function */
 
 static void MUSART1_privateInit(void);
 static void MUSART2_privateInit(void);
 static void MUSART6_privateInit(void);
 
+/*private function to send byte synch*/
 
 static void MUSART1_privateSendByte(u8 copy_u8PrivateData);
 static void MUSART2_privateSendByte(u8 copy_u8PrivateData);
 static void MUSART6_privateSendByte(u8 copy_u8PrivateData);
 
+/*private function to send string synch*/
 
-void MUSART1_PrivateSendStringSynch(s8* copy_Pu8StringToSend);
-void MUSART2_PrivateSendStringSynch(s8* copy_Pu8StringToSend);
-void MUSART6_PrivateSendStringSynch(s8* copy_Pu8StringToSend);
+static void MUSART1_PrivateSendStringSynch(s8* copy_Pu8StringToSend);
+static void MUSART2_PrivateSendStringSynch(s8* copy_Pu8StringToSend);
+static void MUSART6_PrivateSendStringSynch(s8* copy_Pu8StringToSend);
 
 
+/*function help sending bytes asynchronous*/
+
+void MUSART1_TX_Byte(void);
+void MUSART2_TX_Byte(void);
+void MUSART6_TX_Byte(void);
+
+/*function help sending Strings asynchronous*/
+
+void MUSART1_TX_String(void);
+void MUSART2_TX_String(void);
+void MUSART6_TX_String(void);
+
+
+/*function help receiving bytes asynchronous*/
+void MUSART1_RX_Byte(void);
+void MUSART2_RX_Byte(void);
+void MUSART6_RX_Byte(void);
+
+/*function help Receiving Strings asynchronous*/
+
+void MUSART1_RX_String(void);
+void MUSART2_RX_String(void);
+void MUSART6_RX_String(void);
+
+
+/*global 2d array to set baud rate*/
 
 u32 baud_rate[MAX_SPEED_MODES][MAX_BAUDRATES_OPTIONS] = { {B_SAMPLING_8_1200 , B_SAMPLING_8_2400
 , B_SAMPLING_8_9600 , B_SAMPLING_8_19200 , B_SAMPLING_8_38400 , B_SAMPLING_8_57600 , B_SAMPLING_8_115200 } ,
@@ -40,6 +91,7 @@ u32 baud_rate[MAX_SPEED_MODES][MAX_BAUDRATES_OPTIONS] = { {B_SAMPLING_8_1200 , B
 		, B_SAMPLING_16_38400 , B_SAMPLING_16_57600 , B_SAMPLING_16_115200} };
 
 
+/********General Init function ********/
 
 void MUSART_voidInit(USART_SELCTION_t copy_enumUSART){
 	switch(copy_enumUSART){
@@ -57,6 +109,118 @@ void MUSART_voidSendByte(USART_SELCTION_t copy_enumUSART , u8 copy_u8ByteToSend)
 	case USART_6 :  MUSART6_privateSendByte(copy_u8ByteToSend);  break;
 	}
 }
+
+
+void MUSART1_TX_Byte(void){
+	USART1->DR = USART1_global_data_to_send;
+}
+
+void MUSART1_voidSendByteAsynch(u8 copy_u8ByteToSend){
+	USART1_global_data_to_send = copy_u8ByteToSend;
+	MUSART_voidSetCallBack(USART_1, MUSART1_TX_Byte);
+	/*Enable Interrupt*/
+	SET_BIT(USART1->CR1 , TXE);
+}
+
+
+void MUSART2_TX_Byte(void){
+	USART2->DR = USART2_global_data_to_send;
+}
+
+void MUSART2_voidSendByteAsynch(u8 copy_u8ByteToSend){
+	USART2_global_data_to_send = copy_u8ByteToSend;
+	MUSART_voidSetCallBack(USART_2, MUSART2_TX_Byte);
+	/*Enable Interrupt*/
+	SET_BIT(USART2->CR1 , TXE);
+}
+
+
+void MUSART6_TX_Byte(void){
+	USART6->DR = USART6_global_data_to_send;
+}
+
+void MUSART6_voidSendByteAsynch(u8 copy_u8ByteToSend){
+	USART6_global_data_to_send = copy_u8ByteToSend;
+	MUSART_voidSetCallBack(USART_6, MUSART6_TX_Byte);
+	/*Enable Interrupt*/
+	SET_BIT(USART6->CR1 , TXE);
+}
+
+
+void MUSART_voidSendStringSynch(USART_SELCTION_t copy_enumUSART , s8* copy_Pu8StringToSend){
+	switch(copy_enumUSART){
+	case USART_1 :  MUSART1_PrivateSendStringSynch(copy_Pu8StringToSend);  break;
+	case USART_2 :  MUSART2_PrivateSendStringSynch(copy_Pu8StringToSend);  break;
+	case USART_6 :  MUSART6_PrivateSendStringSynch(copy_Pu8StringToSend);  break;
+	}
+}
+
+void MUSART1_TX_String(void){
+	static u8 index = 0;
+		if(USART1_globalStringTX != NULL){
+			if(USART1_globalStringTX[index] != '\0'){
+				USART1->DR = USART1_globalStringTX[index++];
+			}else{
+				index = 0;
+				/*clear interrupt*/
+				CLR_BIT(USART1->CR1 , TXE);
+			}
+		}
+}
+
+void MUSART1_voidSendStringAsynch(s8* copy_Pu8StringToSend){
+	if(copy_Pu8StringToSend != NULL){
+		USART1_globalStringTX = copy_Pu8StringToSend;
+		MUSART_voidSetCallBack(USART_1 , MUSART1_TX_String);
+		SET_BIT(USART1->CR1 , TXE);
+	}
+}
+
+void MUSART2_TX_String(void){
+	static u8 index = 0;
+		if(USART2_globalStringTX != NULL){
+			if(USART2_globalStringTX[index] != '\0'){
+				USART2->DR = USART2_globalStringTX[index++];
+			}else{
+				index = 0;
+				/*clear interrupt*/
+				CLR_BIT(USART2->CR1 , TXE);
+			}
+		}
+}
+
+void MUSART2_voidSendStringAsynch(s8* copy_Pu8StringToSend){
+	if(copy_Pu8StringToSend != NULL){
+		USART2_globalStringTX = copy_Pu8StringToSend;
+		MUSART_voidSetCallBack(USART_2 , MUSART2_TX_String);
+		SET_BIT(USART2->CR1 , TXE);
+	}
+}
+
+
+void MUSART6_TX_String(void){
+	static u8 index = 0;
+		if(USART6_globalStringTX != NULL){
+			if(USART6_globalStringTX[index] != '\0'){
+				USART6->DR = USART6_globalStringTX[index++];
+			}else{
+				index = 0;
+				/*clear interrupt*/
+				CLR_BIT(USART6->CR1 , TXE);
+			}
+		}
+}
+
+void MUSART6_voidSendStringAsynch(s8* copy_Pu8StringToSend){
+	if(copy_Pu8StringToSend != NULL){
+		USART6_globalStringTX = copy_Pu8StringToSend;
+		MUSART_voidSetCallBack(USART_6 , MUSART6_TX_String);
+		SET_BIT(USART6->CR1 , TXE);
+	}
+}
+
+
+/*****Receive Byte Synchronous******/
 
 u8 MUSART1_u8ReceiveByte(void){
 	u8 data;
@@ -81,12 +245,44 @@ u8 MUSART6_u8ReceiveByte(void){
 	return data;
 }
 
-void MUSART_voidSendStringSynch(USART_SELCTION_t copy_enumUSART , s8* copy_Pu8StringToSend){
-	switch(copy_enumUSART){
-	case USART_1 :  MUSART1_PrivateSendStringSynch(copy_Pu8StringToSend);  break;
-	case USART_2 :  MUSART2_PrivateSendStringSynch(copy_Pu8StringToSend);  break;
-	case USART_6 :  MUSART6_PrivateSendStringSynch(copy_Pu8StringToSend);  break;
+
+void MUSART1_RX_Byte(void){
+	if(USART1_globalByteRX != NULL){
+		*(USART1_globalByteRX) = USART1->DR;
 	}
+}
+
+void MUSART1_voidRecieveByteAsynch(u8* data){
+	if(data != NULL )  USART1_globalByteRX = data;
+	MUSART_voidSetCallBack(USART_1, MUSART1_RX_Byte);
+	SET_BIT(USART1->CR1 , RXNE);
+}
+
+
+void MUSART2_RX_Byte(void){
+	if(USART2_globalByteRX != NULL){
+		*(USART2_globalByteRX) = USART2->DR;
+	}
+}
+
+void MUSART2_voidRecieveByteAsynch(u8* data){
+	if(data != NULL )  USART2_globalByteRX = data;
+	MUSART_voidSetCallBack(USART_2, MUSART2_RX_Byte);
+	SET_BIT(USART2->CR1 , RXNE);
+}
+
+
+
+void MUSART6_RX_Byte(void){
+	if(USART6_globalByteRX != NULL){
+		*(USART6_globalByteRX) = USART6->DR;
+	}
+}
+
+void MUSART6_voidRecieveByteAsynch(u8* data){
+	if(data != NULL )  USART6_globalByteRX = data;
+	MUSART_voidSetCallBack(USART_6, MUSART6_RX_Byte);
+	SET_BIT(USART6->CR1 , RXNE);
 }
 
 
@@ -128,18 +324,79 @@ u8 MUSART6_Pu8RecieveStringSynch(s8* word){
 }
 
 
+void MUSART1_RX_String(void){
+	static u8 index = 0;
+	u8 temp = USART1->DR;
+	if(temp != NEXT_LINE){
+		USART1_globalReceiveRX[index] = temp;
+		index++;
+	}else{
+		USART1_globalReceiveRX[index] = '\0';
+		index = 0;
+		CLR_BIT(USART1->CR1 , RXNE);
+	}
+}
+
+void MUSART1_voidRecieveStringAsynch(s8* copy_Pu8StringToRecieve){
+	if(copy_Pu8StringToRecieve != NULL){
+		USART1_globalReceiveRX = copy_Pu8StringToRecieve;
+		MUSART_voidSetCallBack(USART_1 ,MUSART1_RX_String );
+		SET_BIT(USART1->CR1 , RXNE);
+	}
+}
+
+
+void MUSART2_RX_String(void){
+	static u8 index = 0;
+	u8 temp = USART2->DR;
+	if(temp != NEXT_LINE){
+		USART2_globalReceiveRX[index] = temp;
+		index++;
+	}else{
+		USART2_globalReceiveRX[index] = '\0';
+		index = 0;
+		CLR_BIT(USART2->CR1 , RXNE);
+	}
+}
+
+void MUSART2_voidRecieveStringAsynch(s8* copy_Pu8StringToRecieve){
+	if(copy_Pu8StringToRecieve != NULL){
+		USART2_globalReceiveRX = copy_Pu8StringToRecieve;
+		MUSART_voidSetCallBack(USART_2 ,MUSART1_RX_String );
+		SET_BIT(USART2->CR1 , RXNE);
+	}
+}
+
+
+
+void MUSART6_RX_String(void){
+	static u8 index = 0;
+	u8 temp = USART6->DR;
+	if(temp != NEXT_LINE){
+		USART6_globalReceiveRX[index] = temp;
+		index++;
+	}else{
+		USART6_globalReceiveRX[index] = '\0';
+		index = 0;
+		CLR_BIT(USART6->CR1 , RXNE);
+	}
+}
+
+void MUSART6_voidRecieveStringAsynch(s8* copy_Pu8StringToRecieve){
+	if(copy_Pu8StringToRecieve != NULL){
+		USART6_globalReceiveRX = copy_Pu8StringToRecieve;
+		MUSART_voidSetCallBack(USART_6 ,MUSART1_RX_String );
+		SET_BIT(USART6->CR1 , RXNE);
+	}
+}
+
+
 void MUSART_voidSetCallBack(USART_SELCTION_t copy_enumUSART , void(*Copy_Pf)(void)){
 	if(Copy_Pf != NULL){
 		USART_PF[copy_enumUSART] = Copy_Pf;
 	}
 }
 
-void USART1_IRQHandler(void){
-	if(USART_PF != NULL){
-		USART_PF[USART_1]();
-		USART1->SR = 0x0000;
-	}
-}
 
 
 
@@ -293,23 +550,52 @@ static void MUSART6_privateSendByte(u8 copy_u8PrivateData){
 
 
 
-void MUSART1_PrivateSendStringSynch(s8* copy_Pu8StringToSend){
+static void MUSART1_PrivateSendStringSynch(s8* copy_Pu8StringToSend){
 	u8 index = 0;
 	while(copy_Pu8StringToSend[index] != '\0'){
 		MUSART_voidSendByte(USART_1 , copy_Pu8StringToSend[index++]);
 	}
 }
 
-void MUSART2_PrivateSendStringSynch(s8* copy_Pu8StringToSend){
+static void MUSART2_PrivateSendStringSynch(s8* copy_Pu8StringToSend){
 	u8 index = 0;
 	while(copy_Pu8StringToSend[index] != '\0'){
 		MUSART_voidSendByte(USART_2 , copy_Pu8StringToSend[index++]);
 	}
 }
 
-void MUSART6_PrivateSendStringSynch(s8* copy_Pu8StringToSend){
+static void MUSART6_PrivateSendStringSynch(s8* copy_Pu8StringToSend){
 	u8 index = 0;
 	while(copy_Pu8StringToSend[index] != '\0'){
 		MUSART_voidSendByte(USART_6 , copy_Pu8StringToSend[index++]);
+	}
+}
+
+
+
+void USART1_IRQHandler(void){
+	if(USART_PF[USART_1] != NULL){
+		USART_PF[USART_1]();
+		/*clear status register*/
+		USART1->SR = 0x0000;
+	}
+}
+
+
+
+void USART2_IRQHandler(void){
+	if(USART_PF[USART_2] != NULL){
+		USART_PF[USART_2]();
+		/*clear status register*/
+		USART2->SR = 0x0000;
+	}
+}
+
+
+void USART6_IRQHandler(void){
+	if(USART_PF[USART_6] != NULL){
+		USART_PF[USART_6]();
+		/*clear status register*/
+		USART6->SR = 0x0000;
 	}
 }
